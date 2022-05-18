181787102ae6bb768e00ce6baa77c39c23aaaa3e
bord-o <brodylittle011@gmail.com>
2022-05-18 01:14:54 -0400

    got encoding working sortof

---
+++ freqfill.py
@@ -0,0 +1,6 @@
+#!/usr/local/bin/python3
+
+for i in range(121, 257):
+    print(f"({i}, 1),")
+
+
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,23 @@ mod helpers;
 
 fn main() {
     let mut db_index: Vec<u8> = Vec::new();
-    let games = helpers::extract_games("/home/bordo/chess-compression/modern35.uci");
+    let games = helpers::extract_games("/home/bordo/chess-compression/moderndb.uci");
     let mut g_counter = 0;
     for g in &games {
         println!("Encoding game: {}", g_counter);
         db_index.extend(helpers::index_game(&g));
         g_counter += 1;
     }
-
     //let test_index: Vec<u8> = vec![0, 2, 14, 13, 0, 0];
+    let (book, _tree) = encoder::huff_gen();
+    encoder::encode(
+        "/home/bordo/chess-compression/encode_db_test.txt",
+        &db_index,
+        book,
+    );
     //let encoded = encoder::test(&test_index);
     //let t = helpers::generate_huff_weights(db_index);
     //let freqs = format!("{:?}", t);
-    encoder::test(&db_index);
 
     // std::fs::write("/usr/home/bordo/chess-compression/temp.txt", freqs)
     //     .expect("Unable to write file");
